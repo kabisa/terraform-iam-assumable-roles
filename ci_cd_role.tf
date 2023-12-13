1cd9c65a1edbfe9ac1b993ae45bc65ccc4c4a16b
Paul van Lierop <paul.van.lierop@kabisa.nl>
2023-12-13 15:49:04 +0100

    Add example to Readme

--- ci_cd_role.tf
+++ ci_cd_role.tf
@@ -3,8 +3,8 @@
 resource "aws_iam_role" "ci_cd_iam_role" {
   count = var.create_ci_cd_role ? 1 : 0
 
-  name               = "ci_cd_access_role"
-  assume_role_policy = data.aws_iam_policy_document.ci_cd_policy_document[0].json
+  name                = "ci_cd_access_role"
+  assume_role_policy  = data.aws_iam_policy_document.ci_cd_policy_document[0].json
   managed_policy_arns = var.ci_cd_role_managed_policies
 
   dynamic "inline_policy" {
